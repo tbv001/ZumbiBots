1d9148772fedd763f170d2e640ff9bd15d6674cb
tbv001 <sebuahakun123@gmail.com>
2026-07-29 16:27:41 +0700

    Update TakeDamage patch in PlayerMain

--- src/Patches/PlayerMainPatch.cs
+++ src/Patches/PlayerMainPatch.cs
@@ -60,11 +60,20 @@ internal static class PlayerMainPatch
         if (__instance.healthSlow < __instance.MaxHealth * 0.25f)
             __instance.healthSlow = __instance.MaxHealth * 0.25f;
 
-        FXPropsController.instance.SpawnProp(
-            __instance.statusEffects.HasToxic ? FXProp.ID.ToxicBlood : FXProp.ID.Blood1, damage.hitPoint,
-            Quaternion.identity, null, true);
+        if (damage.hitPoint == Vector3.zero)
+            damage.hitPoint = __instance.transform.position + Vector3.up * 0.8f;
+
         AudioController.instance.PlayImpact(__instance.playerAudio.gun.transform.position,
             MeleeImpactAudioID.CleanPunch, true);
+
+        if (damage.damageType == DamageType.Toxic)
+        {
+            SharedParticlesManager.Emit(SharedParticleID.ToxicBlood, 20, damage.hitPoint, true);
+        }
+        else
+        {
+            SharedParticlesManager.Emit(__instance.GetBloodParticles(damage.amount), damage.hitPoint, true);
+        }
 
         if (__instance.healthFast < 0f)
         {
